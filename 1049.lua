5cb03cb2d063712951afab9d7b14a0dec60aee67
CKScrips <levelupp25@yahoo.com>
2024-08-27 23:01:25 -0400

    Update 1049.lua

--- 1049.lua
+++ 1049.lua
@@ -1,8 +1,7 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/
-source')))()
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
-local Window = OrionLib:MakeWindow({Name = "CKScripHub | Adopt Me Hub", HidePremium = false, IntroText = 
-"CKScripHub", SaveConfig = true, ConfigFolder = "AdoptMeConfig"})
+local Window = OrionLib:MakeWindow({Name = "CKScripHub | Adopt Me Hub", HidePremium = false, IntroText = "CKScripHub", SaveConfig = true, 
+ConfigFolder = "AdoptMeConfig"})
 
 -- Values
 _G.AutoMFR = True
@@ -16,22 +15,22 @@ function AutoMFR()
 		require(game.ReplicatedStorage.ClientModules.Core.ClientData)
 		clientData.get_data()[tostring(game.Players.LocalPlayer)]
 		for i,v in pairs(playerData.inventory.pets) do
-	v.properties.rideable = true
-	v.properties.flyable = true
-	v.properties.mega_neon = true
+			v.properties.rideable = true
+	        	v.properties.flyable = true
+	        	v.properties.mega_neon = true
 	  	end
   	end
 end
 
 function AutoNFR()
 	while _G.AutoNFR do
-	wait(.0001)
+		wait(.0001)
 		require(game.ReplicatedStorage.ClientModules.Core.ClientData)
 		clientData.get_data()[tostring(game.Players.LocalPlayer)]
 		for i,v in pairs(playerData.inventory.pets) do
-	v.properties.rideable = true
-	v.properties.flyable = true
-	v.properties.neon = true
+	        	v.properties.rideable = true
+	        	v.properties.flyable = true
+	        	v.properties.neon = true
 	  	end
   	end
 end
@@ -40,11 +39,11 @@ end
 
 -- TextBoxes
 TradeTab:AddTextbox({
-    	Name = "Username",
-    	Default = " ",
-    	TextDisappear = false,
-    	Callback = function()
-    	end      
+    Name = "Username",
+    Default = " ",
+    TextDisappear = false,
+    Callback = function()
+    end      
 })
 
 
@@ -54,13 +53,13 @@ TradeTab:AddTextbox({
 DupeTab:AddButton({
 	Name = "Equip egg to dupe, then click here",
 	Callback = function()
-			OrionLib:MakeNotification({
+		OrionLib:MakeNotification({
 	Name = "Equip Egg To Dupe",
 	Content = "Egg Equipped",
 	Image = "rbxassetid://4483345998",
 	Time = 5})
-		  print("button pressed")
-  end    
+		print("button pressed")
+    	end    
 })
 
 --DupeButton
@@ -80,13 +79,13 @@ DupeTab:AddButton({
 DupeTab:AddButton({
 	Name = "Equip pet to dupe, then click here.",
 	Callback = function()
-			OrionLib:MakeNotification({
+		OrionLib:MakeNotification({
 	Name = "Equip Pet To Dupe",
 	Content = "Pet Equipped",
 	Image = "rbxassetid://4483345998",
 	Time = 5})
-		  print("button pressed")
-	  end    
+		print("button pressed")
+	end    
 })
 
 --DupeButton
@@ -103,10 +102,10 @@ DupeTab:AddButton({
 })
 
 BucksTab:AddButton({
-    Name = "Get Infinite Bucks",
-    Callback = function()
+	Name = "Get Infinite Bucks",
+    	Callback = function()
 		print("button pressed")
-    end    
+    	end    
 })
 
 FarmTab:AddButton({
@@ -119,12 +118,12 @@ FarmTab:AddButton({
 TradeTab:AddButton({
 	Name = "Freeze Trade To The Victim",
 	Callback = function()
-			OrionLib:MakeNotification({
+		OrionLib:MakeNotification({
 	Name = "Freeze Trade Enabled",
 	Content = "Remove your offer. The victim can not see that you've removed items/pets.",
 	Image = "rbxassetid://4483345998",
 	Time = 5})
-			  print("button pressed")
+		print("button pressed")
 	end    
 })
 
@@ -145,21 +144,21 @@ local FarmTab = Window:MakeTab({
 })
 
 local ChangerTab = Window:MakeTab({
-    	Name = "MFR/NFR Pets",
-   	  Icon = "rbxassetid://4483345998",
-    	PremiumOnly = false
+    Name = "MFR/NFR Pets",
+   	Icon = "rbxassetid://4483345998",
+    PremiumOnly = false
 })
 
 local BucksTab = Window:MakeTab({
     Name = "Infinite Bucks",
     Icon = "rbxassetid://4483345998",
-	  PremiumOnly = false
+	PremiumOnly = false
 })
 
 local TradeTab = Window:MakeTab({
 	Name = "Trade Scam",
 	Icon = "rbxassetid://4483345998",
-  PremiumOnly = false
+    PremiumOnly = false
 })
 
 
@@ -169,7 +168,7 @@ FarmTab:AddToggle({
 	Name = "Pet Farm",
 	Default = false,
 	Callback = function(value)
-			print(value)
+		print(value)
 	end
 })
 
@@ -177,7 +176,7 @@ FarmTab:AddToggle({
 	Name = "Baby Farm",
 	Default = false,
 	Callback = function(value)
-			print(value)
+		print(value)
 	end
 })
 
@@ -205,22 +204,19 @@ OrionLib:MakeNotification({
     Name = "Script Successfully Loaded",
     Content = "Loading Has been successful!",
     Image = "rbxassetid://17849312358",
-    Time = 15
-})
+    Time = 10})
 
 
 -- Paragraphs
 TradeTab:AddParagraph("*READ*","The function - Freeze Trade - will not work in private or full servers.")
 
-TradeTab:AddParagraph("cont.","This trade needs to look convincing. You must have pets of value so your 
-victim will accept.")
+TradeTab:AddParagraph("cont.","This trade needs to look convincing. You must have pets of value so your victim will accept.")
 	
 FarmTab:AddParagraph("*READ*","To avoid glitching, do not use pet farm and baby farm at the same time.")
 	
 DupeTab:AddParagraph("*READ*","The Feature - Dupe Eggs/Pets -  will not work in private or full servers.")
 
-ChangerTab:AddParagraph("Pets do not stay MFR/NFR","If you try to trade them, they will show up as normal 
-pets.")
+ChangerTab:AddParagraph("Pets do not stay MFR/NFR","If you try to trade them, they will show up as normal pets.")
 
 Tab:AddParagraph("*NOTE*","Features Do Not Work If You're in a Private or Full Server")
 
